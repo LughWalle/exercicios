fb0cdbf9dbd39570d31c4558c3d2e77fdd825461
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-03 21:38:39 -0300

    Create matrizTabuada.js

---
+++ matrizTabuada.js
@@ -0,0 +1,36 @@
+function matrizTabuada(array) {
+    const matriz = []; // matriz
+    for (let i = 0; i < array.length; i++) {
+        matriz.push(`tabuada de ${array[i]}`); // identificador de  tabuada
+				const resultTabuada = []; // recebera tabuada
+				for (let n = 1; n <= 10; n++) {
+						resultTabuada.push(array[i] * n); // insere cada resultado da tabuada
+				}
+        matriz.push(resultTabuada); // insere cada tabuada na matriz
+    }
+    return matriz;
+}
+
+console.log(matrizTabuada([2, 3, 7]);
+
+// responsabilidades separadas
+
+function tabuada(n) {
+    const resultTabuada = []
+    for (let i = 1; i <= 10; i++) {
+        resultTabuada.push(n * i)
+    }
+		console.log(resultTabuada);
+    return resultTabuada;
+}
+
+function matrizTabuada(array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(`tabuada de ${array[i]}`)
+        result.push(tabuada(array[i]));
+    }
+    return result;
+}
+
+console.log(matrizTabuada([1, 2, 3]));
