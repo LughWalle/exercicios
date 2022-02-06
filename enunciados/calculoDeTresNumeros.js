2806b997f98c7c8bd5205ec94db9d3ce779f9fe5
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-06 17:00:39 -0300

    Create calculoDeTresNumeros.js

---
+++ enunciados/calculoDeTresNumeros.js
@@ -0,0 +1,8 @@
+// Nível L
+// Crie uma função com as seguintes características:
+// 1. A função deve receber 3 argumentos.
+// 2. Se somente um argumento for passado, retorne o valor do argumento.
+// 3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+// 4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+// 5. Se nenhum argumento for passado, retorne o valor booleano false.
+/// 6. E ainda, se nenhuma das condições acima forem atendidas, retorne null.
