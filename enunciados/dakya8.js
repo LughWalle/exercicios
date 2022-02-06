4aa29487963908fe3a4ceb98298e6fe01875beb8
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-06 10:51:17 -0300

    Update dakya8.js

--- enunciados/dakya8.js
+++ enunciados/dakya8.js
@@ -1,6 +1,9 @@
+// Nível L
 // Crie uma função recebe dois paramentro, diaDaSemana e quantidadeDeDias.
 // Ela deve contar os dias apartir do primeiro paramentro ate a quantidade
-// recebida no segundo parametro, o retornando deve ser o dia correspondente ao final da contagem
+// recebida no segundo parametro, o retorno deve ser o dia correspondente ao final da contagem
+
 // Exemplo: Os parâmetros passados são "Terça-feira" e 5. A função deverá avançar cinco dias e retornar "Domingo".
+
 // utilize essa variavel para os dias:
 // let diasSemana = ["Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado", "Domingo"];
