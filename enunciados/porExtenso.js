9d73790dd1ecc06ccb5f2a65ab344334e6e30ce5
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-05 23:52:59 -0300

    Create porExtenso.js

---
+++ enunciados/porExtenso.js
@@ -0,0 +1,9 @@
+// Desafio em construção, enunciado incompleto, aguarde
+
+// Criar um algoritmo que ao receber um valor retorna uma String com ele por extenso. 
+
+// Exemplo:
+
+// Entrada > 25
+
+// Saída > "Vinte e cinco"
