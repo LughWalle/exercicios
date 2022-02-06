e3301f2c7dbc53964cb9cfa4f0306aa5d684e890
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-06 00:30:51 -0300

    new challenge

---
+++ enunciados/clearUpTheChaos.js
@@ -0,0 +1,17 @@
+// Parece que algo aconteceu com essas strings. Você consegue descobrir como limpar o caos?
+// Escreva uma função que recebe dois parametros
+// O primeiro com o caracter % no meio da palavra
+// O segundo a palavra deve ser invertida e com o caracter %
+// Remova as % e reorganize o segundo parametro
+
+// exemplos:
+// entrada > 'java', 'tpi%rcs' | saida > 'Javascript'
+
+// entrada > 'c%ountry', 'edis' | saida > 'Countryside'
+
+// entrada > 'down', 'nw%ot' | saida > 'Downtown'
+
+// Una essas strings de forma que elas formem as seguintes palavras: 'Javascript', 'Countryside' e 'Downtown'
+
+
+// Dicas: Você pode dar uma olhada nos métodos básicos de string JS como replace(), split(), slice() etc
--- enunciados/porExtenso.js
+++ enunciados/porExtenso.js
@@ -1,6 +1,6 @@
-// Desafio em construção, enunciado incompleto, aguarde
-
-// Criar um algoritmo que ao receber um valor retorna uma String com ele por extenso. 
+// nivel C
+ 
+// Criar um algoritmo que ao receber um valor retorna uma String com ele por extenso.
 
 // Exemplo:
 
