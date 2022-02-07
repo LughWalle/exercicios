b12b9a88c9d00b65963db86243336d26704ac4fa
Lucival dos Santos França Filho <48294395+LughWalle@users.noreply.github.com>
2022-02-07 15:24:28 -0300

    Create numerosNegativoPositivos.js

---
+++ enunciados/numerosNegativoPositivos.js
@@ -0,0 +1,4 @@
+// crie uma função que receba um array de numeros
+// e imprima na tela os números positivos e negativos,
+// para valor zero imprima na tela "O número 0 é um número neutro!" 
+// OBS: O retorno deverá ser um array.
